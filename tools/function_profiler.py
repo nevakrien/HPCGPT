63e1ad1c24c5db3a164a319f66107e61344ec73f
User <nevo.krien@gmail.com>
2024-01-24 22:10:35 +0200

    full benchmark achived

--- tools/function_profiler.py
+++ tools/function_profiler.py
@@ -81,6 +81,20 @@ def print_call_stack(call_data, level=0):
     for inner_call in call_data.inner_calls:
         print_call_stack(inner_call, level + 1)
 
+def format_large_number(n):
+    if n < 1000:
+        return str(n)
+    elif n < 1e6:
+        return f'{n / 1e3:.1f}k'
+    elif n < 1e9:
+        return f'{n / 1e6:.1f}M'
+    elif n < 1e12:
+        return f'{n / 1e9:.1f}B'
+    elif n < 1e15:
+        return f'{n / 1e12:.1f}T'
+    else:
+        return f'{n:.1e}'
+
 #PROFILE
 def _calculate_time(calls,funcs):
     total_time=0
@@ -108,6 +122,7 @@ class CallIntevals():
     def __init__(self,function):
         self.function=function
         self.inner_calls=[]
+        self.perf_data=[]
         self.i=0
 
 
@@ -121,20 +136,20 @@ class CallIntevals():
         #print(f"got {t} in func {self.function.name}")
         
         if(self.i==len(self.inner_calls)):
-            print(f"no call stack remaining returining {self.function.name}")
+            #print(f"no call stack remaining returining {self.function.name}")
             return self
 
         c=self.inner_calls[self.i]
         if c.function.start>t:
-            print(f"inner func {c.function.name} is too late with {c.function.start}returning {self.function.name}")
+            #print(f"inner func {c.function.name} is too late with {c.function.start}returning {self.function.name}")
             return self
         
         if(t>c.function.end):
-            print(f"inner func {c.function.name} is too early returning incrementing")
+            #print(f"inner func {c.function.name} is too early returning incrementing")
             self.i+=1
             return self.get_atributed(t)
 
-        print(f"inner func {c.function.name} is valid calling it")
+        #print(f"inner func {c.function.name} is valid calling it")
         return c.get_atributed(t)
 
 
@@ -157,14 +172,41 @@ def get_perfstuff(call,perf_data):
 
     perf_data.sort(key=lambda x: x.timestamp)
 
-    for p in perf_data[-10:]:
+    for p in perf_data:
         a=call.get_atributed(p.timestamp)
         #print(call.i)
         data[a.function.name].append(p)
+        a.perf_data.append(p)
 
-    return data
+    return data,call
+
+@dataclass
+class PIDTime():
+    pid_count :int
+    duration :float
 
 
+def _get_process_data(call,funcs=None):
+    if funcs is None:
+        funcs=defaultdict(lambda:[])
+
+    count=len(set(x.process_id for x in call.perf_data))
+    duration=call.function.end-call.function.start
+
+    funcs[call.function.name].append(PIDTime(count,duration))
+
+    for c in call.inner_calls:
+        _get_process_data(c,funcs)
+    
+    return funcs
+
+def _get_process_stats(l):
+    return {'avg process count':sum(x.pid_count for x in l)/len(l),
+            'avg run duration':sum(x.duration for x in l)/len(l)}
+
+def get_process_info(call):
+    d=_get_process_data(call)
+    return {k:_get_process_stats(v) for k,v in d.items()}
 
 
 #Validation:
@@ -177,15 +219,10 @@ def check_processed(call):
 
     return all(map(check_processed,call.inner_calls))
 
-def align_timers(raw_logs,perf_samples):
-    min_sample=(min(x.timestamp for x in perf_samples))
-    min_func=(min(x.seconds for x in raw_logs))
-
-    offset=min_func-min_sample
-    for log in raw_logs:
-        log.seconds-=offset
 
 if __name__ == "__main__":
+    pd.set_option('display.float_format', lambda x: '%.6f' % x)
+
     file_path=join('results','combo')
 
     perf_samples=r_sample.parse_file(join(file_path,'output.txt'))
@@ -198,8 +235,8 @@ if __name__ == "__main__":
 
     print(check_processed(call_stack[0]))
 
-    print(type(call_stack[0].function))
-    print(len(call_stack))
+    #print(type(call_stack[0].function))
+    #print(len(call_stack))
     
 
     max_sample=(max(x.timestamp for x in perf_samples))
@@ -216,18 +253,42 @@ if __name__ == "__main__":
     #perf_samples.sort(key=lambda x: x.timestamp)
     
     assert len(call_stack)==1
+    assert call_stack[0].function.name=='program'
 
     #print(print_call_stack(call_stack[0]))
     #print(calculate_time(call_stack))
     times=calculate_time(call_stack)
     total_time=sum(times.values())
-    df={k:[v,v/total_time] for k,v in times.items()}
-    df=pd.DataFrame(df,index=['time spend','frac'])
+    df={k:[v,100*v/total_time] for k,v in times.items()}
+    df=pd.DataFrame(df,index=['time spend','%'+'of runtime'])
     #df['frac']=df['time spend']/df['time spend'].sum()
-    print(df)
+    #print(df)
 
     print(2*'\n')
 
-    perf_info=get_perfstuff(call_stack[0],perf_samples)
-    print(perf_info.keys())
+    perf_info,call=get_perfstuff(call_stack[0],perf_samples)
+    #print(perf_info.keys())
+
+    cycles={k:(sum(x.cycles for x in v)) for k,v in perf_info.items()}
+    
+
+    #df2={k:format_large_number(v) for k,v in cycles.items()}
+    df2=cycles#{k:v for k,v in cycles.items()} 
+    df2=pd.DataFrame(df2,index=['cycles'])
+
+    df2=df2.transpose()
+    #print(df2.applymap(format_large_number))
+    df2['cycles per second']=df2['cycles']/df.transpose()['time spend']
+    
+    df2=df2.transpose().applymap(format_large_number)
+
+    df2=pd.concat([df.applymap(lambda x: f'{x:.6f}'),df2])
+    #print(df2)
+
+    df3=get_process_info(call)
+    df3=pd.DataFrame(df3)
+
+    df3=pd.concat([df2,df3])
+
+    print(df3)
 
